1dd50396a6bc9ee843d2ed134a73178015ad1596
Nikolaj-Storm <121698062+Nikolaj-Storm@users.noreply.github.com>
2025-06-18 20:43:26 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/rouge-beige-nexus/", // ✅ Needed for GitHub Pages
+  base: "/rouge-beige-nexus/", // 👈 Needed for GitHub Pages
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
